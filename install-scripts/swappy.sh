b838c320a8248d1c8895e378e69e608e88f955ed
Ja.KooLit <85185940+JaKooLit@users.noreply.github.com>
2024-06-05 14:56:40 +0900

    Update swappy.sh

--- install-scripts/swappy.sh
+++ install-scripts/swappy.sh
@@ -32,7 +32,15 @@ for PKG1 in "${swappy[@]}"; do
   fi
 done
 
-##
+# Force reinstall above as seems its giving issue as swappy cant be build
+for PKG1 in "${swappy[@]}"; do
+  sudo apt --reinstall install "$PKG1" 2>&1 | tee -a "$LOG"
+  if [ $? -ne 0 ]; then
+    echo -e "\e[1A\e[K${ERROR} - $PKG1 Package installation failed, Please check the installation logs"
+    exit 1
+  fi
+done
+
 printf "${NOTE} Installing swappy from source...\n"  
 
 # Check if folder exists and remove it
