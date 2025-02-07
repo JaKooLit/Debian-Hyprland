ca3469199808c4224ef3ee0156cf37d656aaf74c
JaKooLit <ejhay.games@gmail.com~>
2025-02-07 19:18:06 +0900

    updated preset.sh

--- preset.sh
+++ preset.sh
@@ -4,7 +4,7 @@
 # Use only Y for Yes and N for No . If you put other letter, the script will fail
 
 
-###-Configure NVIDIA
+###-Would you like script to Configure NVIDIA for you?
 nvidia="N"
 
 ###-Install GTK themes (required for Dark/Light function)?
@@ -22,6 +22,7 @@ thunar_choice="Y"
 ### Adding user to the 'input' group might be necessary for waybar keyboard-state functionality
 input_group="Y"
 
+### Desktop overview Demo Link in README
 ### Install AGS (aylur's GTK shell) v1 for Desktop-Like Overview?" 
 ags="Y"
 
@@ -33,12 +34,14 @@ sddm_theme="Y"
 ###-Install XDG-DESKTOP-PORTAL-HYPRLAND? (For proper Screen Share ie OBS)
 xdph="Y"
 
+### Shell extension. Bash alternative
 ###-Install zsh, oh-my-zsh
 zsh="Y"
 
 ### add Pokemon color scripts to terminal
 pokemon_choice="Y"
 
+### This will install ASUSCTL & SUPERGFXCTL
 ###-Installing on Asus ROG Laptops?
 rog="N"
 
