42cbae3c1c4ed6d1a662685aae96905fb0a87079
Don Williams <don.e.williams@gmail.com>
2025-12-10 12:35:43 -0500

    Pushing sdegler's changes to dev
     On branch development
     Your branch is up to date with 'origin/development'.
    
     Changes to be committed:
    	modified:   install-scripts/aquamarine.sh
    	new file:   install-scripts/xkbcommon.sh

--- install-scripts/aquamarine.sh
+++ install-scripts/aquamarine.sh
@@ -45,7 +45,7 @@ fi
 printf "${INFO} Installing ${YELLOW}aquamarine $tag${RESET} ...\n"
 if git clone --recursive -b $tag https://github.com/hyprwm/aquamarine.git; then
     cd aquamarine || exit 1
-	cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build
+	cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr/local -S . -B ./build
 	cmake --build ./build --config Release --target all -j`nproc 2>/dev/null || getconf NPROCESSORS_CONF`
     if [ $DO_INSTALL -eq 1 ]; then
         if sudo cmake --install ./build 2>&1 | tee -a "$MLOG" ; then
