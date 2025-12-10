24fb2b0dc5598c6b98dab0228c335e298320db77
Don Williams <don.e.williams@gmail.com>
2025-12-10 13:17:36 -0500

    Fixed pkg name qt-style-kavantum-themes not qt5
     On branch development
     Your branch is up to date with 'origin/development'.
    
     Changes to be committed:
    	modified:   install-scripts/01-hypr-pkgs.sh

--- install-scripts/01-hypr-pkgs.sh
+++ install-scripts/01-hypr-pkgs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 💫 https://github.com/JaKooLit 💫 #
 # Hyprland-Dots Packages #
-# edit your packages desired here. 
+# edit your packages desired here.
 # WARNING! If you remove packages here, dotfiles may not work properly.
 # and also, ensure that packages are present in Debian Official Repo
 
@@ -12,79 +12,82 @@ Extra=(
 
 # packages needed
 hypr_package=(
-  cliphist
-  grim
-  gvfs
-  gvfs-backends
-  inxi
-  imagemagick
-  kitty
-  nano
-  pavucontrol
-  playerctl
-  polkit-kde-agent-1
-  python3-requests
-  python3-pip
-  qt5ct
-  qt5-style-kvantum
-  qt5-style-kvantum-themes
-  qt6ct
-  slurp
-  swappy
-  sway-notification-center
-  unzip
-  waybar
-  wget
-  wl-clipboard
-  wlogout
-  xdg-user-dirs
-  xdg-utils
-  yad
+    cliphist
+    grim
+    gvfs
+    gvfs-backends
+    inxi
+    imagemagick
+    kitty
+    nano
+    pavucontrol
+    playerctl
+    polkit-kde-agent-1
+    python3-requests
+    python3-pip
+    qt5ct
+    qt5-style-kvantum
+    qt-style-kvantum-themes
+    qt6ct
+    slurp
+    swappy
+    sway-notification-center
+    unzip
+    waybar
+    wget
+    wl-clipboard
+    wlogout
+    xdg-user-dirs
+    xdg-utils
+    yad
 )
 
 # the following packages can be deleted. however, dotfiles may not work properly
 hypr_package_2=(
-  brightnessctl
-  btop
-  cava
-  fastfetch
-  loupe
-  gnome-system-monitor
-  mousepad
-  mpv
-  mpv-mpris
-  nwg-look
-  nwg-displays
-  nvtop
-  pamixer
-  qalculate-gtk
+    brightnessctl
+    btop
+    cava
+    fastfetch
+    loupe
+    gnome-system-monitor
+    mousepad
+    mpv
+    mpv-mpris
+    nwg-look
+    nwg-displays
+    nvtop
+    pamixer
+    qalculate-gtk
 )
 
-# packages to force reinstall 
+# packages to force reinstall
 force=(
-  imagemagick
-  wayland-protocols
+    imagemagick
+    wayland-protocols
 )
 
 # List of packages to uninstall as it conflicts with swaync or causing swaync to not function properly
 uninstall=(
-  dunst
-  mako
-  rofi
-  cargo
+    dunst
+    mako
+    rofi
+    cargo
 )
 
 ## WARNING: DO NOT EDIT BEYOND THIS LINE IF YOU DON'T KNOW WHAT YOU ARE DOING! ##
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Change the working directory to the parent directory of the script
 PARENT_DIR="$SCRIPT_DIR/.."
-cd "$PARENT_DIR" || { echo "${ERROR} Failed to change directory to $PARENT_DIR"; exit 1; }
+cd "$PARENT_DIR" || {
+    echo "${ERROR} Failed to change directory to $PARENT_DIR"
+    exit 1
+}
 
 # Source the global functions script
 if ! source "$(dirname "$(readlink -f "$0")")/Global_functions.sh"; then
-  echo "Failed to source Global_functions.sh"
-  exit 1
+    echo "Failed to source Global_functions.sh"
+    exit 1
 fi
 
 # Set the name of the log file to include the current date and time
@@ -94,14 +97,14 @@ LOG="Install-Logs/install-$(date +%d-%H%M%S)_hypr-pkgs.log"
 overall_failed=0
 printf "\n%s - ${SKY_BLUE}Removing some packages${RESET} as it conflicts with KooL's Hyprland Dots \n" "${NOTE}"
 for PKG in "${uninstall[@]}"; do
-  uninstall_package "$PKG" 2>&1 | tee -a "$LOG"
-  if [ $? -ne 0 ]; then
-    overall_failed=1
-  fi
+    uninstall_package "$PKG" 2>&1 | tee -a "$LOG"
+    if [ $? -ne 0 ]; then
+        overall_failed=1
+    fi
 done
 
 if [ $overall_failed -ne 0 ]; then
-  echo -e "${ERROR} Some packages failed to uninstall. Please check the log."
+    echo -e "${ERROR} Some packages failed to uninstall. Please check the log."
 fi
 
 printf "\n%.0s" {1..1}
@@ -110,24 +113,24 @@ printf "\n%.0s" {1..1}
 printf "\n%s - Installing ${SKY_BLUE}KooL's hyprland necessary packages${RESET} .... \n" "${NOTE}"
 
 for PKG1 in "${hypr_package[@]}" "${hypr_package_2[@]}" "${Extra[@]}"; do
-  install_package "$PKG1" "$LOG"
+    install_package "$PKG1" "$LOG"
 done
 
 printf "\n%.0s" {1..1}
 
 for PKG2 in "${force[@]}"; do
-  re_install_package "$PKG2" "$LOG"
+    re_install_package "$PKG2" "$LOG"
 done
 
 printf "\n%.0s" {1..1}
 # install YAD from assets. NOTE This is downloaded from SID repo and sometimes
 # Trixie is removing YAD for some strange reasons
 # Check if yad is installed
-if ! command -v yad &> /dev/null; then
-  echo "${INFO} Installing ${YELLOW}YAD from assets${RESET} ..."
-  sudo dpkg -i assets/yad_0.40.0-1+b2_amd64.deb
-  sudo apt install -f -y
-  echo "${INFO} ${YELLOW}YAD from assets${RESET} succesfully installed ..."
+if ! command -v yad &>/dev/null; then
+    echo "${INFO} Installing ${YELLOW}YAD from assets${RESET} ..."
+    sudo dpkg -i assets/yad_0.40.0-1+b2_amd64.deb
+    sudo apt install -f -y
+    echo "${INFO} ${YELLOW}YAD from assets${RESET} succesfully installed ..."
 fi
 
 printf "\n%.0s" {1..2}
@@ -141,3 +144,4 @@ source "$HOME/.cargo/env"
 sudo chmod +s $(which brightnessctl) 2>&1 | tee -a "$LOG" || true
 
 printf "\n%.0s" {1..2}
+
