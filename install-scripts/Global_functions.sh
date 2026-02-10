10a72bdd1e0d82a2432fc96c85c2fd57ecc867b5
Don Williams <don.e.williams@gmail.com>
2026-02-10 11:41:53 -0500

    Updated Global_functions
     On branch development
     Your branch is up to date with 'origin/development'.
    
     Changes to be committed:
    	modified:   install-scripts/Global_functions.sh

--- install-scripts/Global_functions.sh
+++ install-scripts/Global_functions.sh
@@ -4,21 +4,26 @@
 
 set -e
 
-# Set some colors for output messages
-OK="$(tput setaf 2)[OK]$(tput sgr0)"
-ERROR="$(tput setaf 1)[ERROR]$(tput sgr0)"
-NOTE="$(tput setaf 3)[NOTE]$(tput sgr0)"
-INFO="$(tput setaf 4)[INFO]$(tput sgr0)"
-WARN="$(tput setaf 1)[WARN]$(tput sgr0)"
-CAT="$(tput setaf 6)[ACTION]$(tput sgr0)"
-MAGENTA="$(tput setaf 5)"
-ORANGE="$(tput setaf 214)"
-WARNING="$(tput setaf 1)"
-YELLOW="$(tput setaf 3)"
-GREEN="$(tput setaf 2)"
-BLUE="$(tput setaf 4)"
-SKY_BLUE="$(tput setaf 6)"
-RESET="$(tput sgr0)"
+# Set some colors for output messages (be resilient in non-interactive shells)
+if tput sgr0 >/dev/null 2>&1; then
+  OK="$(tput setaf 2)[OK]$(tput sgr0)"
+  ERROR="$(tput setaf 1)[ERROR]$(tput sgr0)"
+  NOTE="$(tput setaf 3)[NOTE]$(tput sgr0)"
+  INFO="$(tput setaf 4)[INFO]$(tput sgr0)"
+  WARN="$(tput setaf 1)[WARN]$(tput sgr0)"
+  CAT="$(tput setaf 6)[ACTION]$(tput sgr0)"
+  MAGENTA="$(tput setaf 5)"
+  ORANGE="$(tput setaf 214)"
+  WARNING="$(tput setaf 1)"
+  YELLOW="$(tput setaf 3)"
+  GREEN="$(tput setaf 2)"
+  BLUE="$(tput setaf 4)"
+  SKY_BLUE="$(tput setaf 6)"
+  RESET="$(tput sgr0)"
+else
+  OK="[OK]"; ERROR="[ERROR]"; NOTE="[NOTE]"; INFO="[INFO]"; WARN="[WARN]"; CAT="[ACTION]"
+  MAGENTA=""; ORANGE=""; WARNING=""; YELLOW=""; GREEN=""; BLUE=""; SKY_BLUE=""; RESET=""
+fi
 
 # Create Directory for Install Logs
 if [ ! -d Install-Logs ]; then
